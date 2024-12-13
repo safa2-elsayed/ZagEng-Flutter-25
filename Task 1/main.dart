b3262df0d8f3aef1c4cd97ecca2f5e2a64ca87dd
Safaa Elsayed <elsayedsafaa696@gmail.com>
2024-12-13 23:04:43 +0200

    Add files via upload

--- Task 1/main.dart
+++ Task 1/main.dart
@@ -1,5 +1,10 @@
 void main(){
+  double distance =25.0 ;
+  double speed = 40.0 ;
+  double Time = distance / speed ;
+  int hours= Time.toInt();
+  int minutes= ((Time -hours) *60).toInt() ;
+  int seconds= (((Time -hours)*60-minutes)*60).toInt() ;
 
-print("Hello I'm \"ZagEng\" ");
-print('Hello I\'m "ZagEng" ');
+print ("To reach the office you spend $hours Hours, $minutes Minutes, $seconds Seconds");
 }
