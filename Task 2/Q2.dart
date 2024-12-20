be471995bc1319155a6f8ab14e95d0a2912df908
Safaa Elsayed <elsayedsafaa696@gmail.com>
2024-12-20 22:38:59 +0200

    Add files via upload

---
+++ Task 2/Q2.dart
@@ -0,0 +1,14 @@
+void main(){
+  var char='p';
+  switch(char) {
+    case 'a' :
+    case 'i' :
+    case 'e' :
+    case 'o' :
+    case 'u' :
+      print ("$char is a Vowel Character" );
+    default :
+      print ("$char is a Consonant Character" );
+  }
+
+}
